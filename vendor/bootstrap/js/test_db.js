a37be09e86addb5731a4f56c3bc42ad83466c1c3
yuaner1106 <3159966363@qq.com>
2023-04-22 08:17:07 +0800

    test db

---
+++ vendor/bootstrap/js/test_db.js
@@ -0,0 +1,254 @@
+function openDB(dbName, version = 1) {
+  return new Promise((resolve, reject) => {
+    var indexedDB =
+      window.indexedDB ||
+      window.mozIndexedDB ||
+      window.webkitIndexedDB ||
+      window.msIndexedDB;
+
+    let db;
+    // open indexedDB if exists otherwise create a new one
+    const request = indexedDB.open(dbName, version);
+
+    // Open database successfully
+    request.onsuccess = function (event) {
+      db = event.target.result; // 数据库对象
+      console.log("Open DataBase Successfully!");
+      resolve(db);
+    };
+
+    // Fail to open this database
+    request.onerror = function (event) {
+      console.log("Error in Open!");
+    };
+
+    // Create or update this database successfully
+    request.onupgradeneeded = function (event) {
+      console.log("onupgradeneeded");
+      db = event.target.result;
+      var objectStore;
+      // Create store
+      objectStore = db.createObjectStore("signalChat", {
+        keyPath: "sequenceId",
+        // autoIncrement: true
+      });
+      // Create indexes
+      objectStore.createIndex("link", "link", { unique: false });
+      objectStore.createIndex("sequenceId", "sequenceId", { unique: false });
+      objectStore.createIndex("messageType", "messageType", {
+        unique: false,
+      });
+    };
+  });
+}
+
+/**
+ * 新增数据
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ * @param {string} data 数据
+ */
+function addData(db, storeName, data) {
+  var request = db
+    .transaction([storeName], "readwrite") // 事务对象 指定表格名称和操作模式（"只读"或"读写"）
+    .objectStore(storeName) // 仓库对象
+    .add(data);
+
+  request.onsuccess = function (event) {
+    console.log("数据写入成功");
+  };
+
+  request.onerror = function (event) {
+    console.log("数据写入失败");
+  };
+}
+
+/**
+ * 新增数据
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ * @param {string} data 数据
+ */
+function addData(db, storeName, data) {
+  var request = db
+    .transaction([storeName], "readwrite") // 事务对象 指定表格名称和操作模式（"只读"或"读写"）
+    .objectStore(storeName) // 仓库对象
+    .add(data);
+
+  request.onsuccess = function (event) {
+    console.log("数据写入成功");
+  };
+
+  request.onerror = function (event) {
+    console.log("数据写入失败");
+  };
+}
+
+/**
+ * 通过索引和游标查询记录
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ * @param {string} indexName 索引名称
+ * @param {string} indexValue 索引值
+ */
+function cursorGetDataByIndex(db, storeName, indexName, indexValue) {
+  let list = [];
+  var store = db.transaction(storeName, "readwrite").objectStore(storeName); // 仓库对象
+  var request = store
+    .index(indexName) // 索引对象
+    .openCursor(IDBKeyRange.only(indexValue)); // 指针对象
+  request.onsuccess = function (e) {
+    var cursor = e.target.result;
+    if (cursor) {
+      // 必须要检查
+      list.push(cursor.value);
+      cursor.continue(); // 遍历了存储对象中的所有内容
+    } else {
+      console.log("游标索引查询结果：", list);
+    }
+  };
+  request.onerror = function (e) {};
+}
+
+/**
+ * 通过索引和游标分页查询记录
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ * @param {string} indexName 索引名称
+ * @param {string} indexValue 索引值
+ * @param {number} page 页码
+ * @param {number} pageSize 查询条数
+ */
+function cursorGetDataByIndexAndPage(
+  db,
+  storeName,
+  indexName,
+  indexValue,
+  page,
+  pageSize
+) {
+  let list = [];
+  let counter = 0; // 计数器
+  let advanced = true; // 是否跳过多少条查询
+  var store = db.transaction(storeName, "readwrite").objectStore(storeName); // 仓库对象
+  var request = store
+    .index(indexName) // 索引对象
+    .openCursor(IDBKeyRange.only(indexValue)); // 指针对象
+  request.onsuccess = function (e) {
+    var cursor = e.target.result;
+    if (page > 1 && advanced) {
+      advanced = false;
+      cursor.advance((page - 1) * pageSize); // 跳过多少条
+      return;
+    }
+    if (cursor) {
+      // 必须要检查
+      list.push(cursor.value);
+      counter++;
+      if (counter < pageSize) {
+        cursor.continue(); // 遍历了存储对象中的所有内容
+      } else {
+        cursor = null;
+        console.log("分页查询结果", list);
+      }
+    } else {
+      console.log("分页查询结果", list);
+    }
+  };
+  request.onerror = function (e) {};
+}
+
+/**
+ * 更新数据
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ * @param {object} data 数据
+ */
+function updateDB(db, storeName, data) {
+  var request = db
+    .transaction([storeName], "readwrite") // 事务对象
+    .objectStore(storeName) // 仓库对象
+    .put(data);
+
+  request.onsuccess = function () {
+    console.log("数据更新成功");
+  };
+
+  request.onerror = function () {
+    console.log("数据更新失败");
+  };
+}
+
+/**
+ * 通过主键删除数据
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ * @param {object} id 主键值
+ */
+function deleteDB(db, storeName, id) {
+  var request = db
+    .transaction([storeName], "readwrite")
+    .objectStore(storeName)
+    .delete(id);
+
+  request.onsuccess = function () {
+    console.log("数据删除成功");
+  };
+
+  request.onerror = function () {
+    console.log("数据删除失败");
+  };
+}
+
+/**
+ * 通过索引和游标删除指定的数据
+ * @param {object} db 数据库实例
+ * @param {string} storeName 仓库名称
+ * @param {string} indexName 索引名
+ * @param {object} indexValue 索引值
+ */
+function cursorDelete(db, storeName, indexName, indexValue) {
+  var store = db.transaction(storeName, "readwrite").objectStore(storeName);
+  var request = store
+    .index(indexName) // 索引对象
+    .openCursor(IDBKeyRange.only(indexValue)); // 指针对象
+  request.onsuccess = function (e) {
+    var cursor = e.target.result;
+    var deleteRequest;
+    if (cursor) {
+      deleteRequest = cursor.delete(); // 请求删除当前项
+      deleteRequest.onerror = function () {
+        console.log("游标删除该记录失败");
+      };
+      deleteRequest.onsuccess = function () {
+        console.log("游标删除该记录成功");
+      };
+      cursor.continue();
+    }
+  };
+  request.onerror = function (e) {};
+}
+
+/**
+ * 关闭数据库
+ * @param {object} db 数据库实例
+ */
+function closeDB(db) {
+  db.close();
+  console.log("数据库已关闭");
+}
+
+/**
+ * 删除数据库
+ * @param {object} dbName 数据库名称
+ */
+function deleteDBAll(dbName) {
+  console.log(dbName);
+  let deleteRequest = window.indexedDB.deleteDatabase(dbName);
+  deleteRequest.onerror = function (event) {
+    console.log("删除失败");
+  };
+  deleteRequest.onsuccess = function (event) {
+    console.log("删除成功");
+  };
+}
